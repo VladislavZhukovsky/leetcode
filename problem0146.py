ff1a25c8e604fb3792a355ccf9fec7a5037796cd
Vladislav Zhukouski <Vladislav_Zhukouski@epam.com>
2022-10-31 12:00:39 +0300

    problem0146 [not finished]

--- problem0146.py
+++ problem0146.py
@@ -1,5 +1,6 @@
 class Node:
-    def __init__(self, value: int, next = None, prev = None):
+    def __init__(self, key: int, value: int, next = None, prev = None):
+        self.key = key
         self.value = value
         self.next = next
         self.prev = prev
@@ -10,17 +11,38 @@ class DLL:
 
     def insertFirst(self, node: Node, removeLast = False):
         if self.head:
-            node.next = self.head
-            node.prev = self.head.prev
-            self.head.prev = node
-            self.head = node
+            if removeLast:
+                nodeLast = self.head.prev
+                node.next = self.head#I
+                self.head.prev = node#II
+                node.prev = nodeLast.prev #III
+                nodeLast.prev.next = node
+                self.head = node
+                return nodeLast
+            else:
+                nodeLast = self.head.prev
+                node.next = self.head
+                self.head.prev = node
+                node.prev = nodeLast
+                nodeLast.next = node
+                self.head = node
         else:
-            self.head = node
             node.next = node
             node.prev = node
+            self.head = node
+
+
+    def move(self, node: Node):
+        if node is self.head:
+            return
+        if node is self.head.prev:
+            self.head = node
+            return
+        node.next.prev = node.prev
+        node.prev.next = node.next
+        self.insertFirst(node, False)
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.hashtable = {}
         self.dll = DLL()
@@ -29,22 +51,35 @@ class LRUCache:
         pass
 
     def get(self, key: int) -> int:
-        if self.hashtable[key]:
-            return self.hashtable[key].value
+        node = self.hashtable.get(key)
+        if node:
+            return node.value
         else:
-            return None
-        pass
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        node = self.hashtable[key]
+        node = self.hashtable.get(key)
         if node:
             node.value = value
-            node = self.dll.remove(node)
-            self.dll.insertFirst(node)
+            self.dll.move(node)
         else:
+            node = Node(key, value)
+            self.hashtable[key] = node
             if self.count < self.capacity:
-                self.hashtable[key] = value
-                node = Node(value)
-                self.dll.insertFirst(node)
+                self.dll.insertFirst(node, False)   
             else:
+                nodeLast = self.dll.insertFirst(node, True)
+                self.hashtable.pop(nodeLast.key)
+            self.count = self.count + 1
+        
 
+lRUCache = LRUCache(2)
+lRUCache.put(1, 1)
+lRUCache.put(2, 2)
+print(lRUCache.get(1))
+lRUCache.put(3, 3)
+print(lRUCache.get(2))
+lRUCache.put(4, 4)
+print(lRUCache.get(1))
+print(lRUCache.get(3))
+print(lRUCache.get(4))
